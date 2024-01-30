ca54081e2ca5d21c1f7181b90559fb449bb1a713
Timurbm <boshayevtm@gmail.com>
2024-01-30 17:02:38 +0600

    first remote push

---
+++ main.py
@@ -0,0 +1,4 @@
+with open('test.txt', 'w') as file:
+    src = file.write('git init - local rep, git add . - add files, git commit -m "" -commit, git checkout -b dev - change branch, git branch - check branches')
+
+#print(src)
